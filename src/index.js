a6b569b7aee86aba662076f62e7df93797f15415
allyson-y <ayao20@holynames-sea.org>
2022-02-09 15:34:26 -0800

    changed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const data = [
+    {
+        id: 512,
+        name: "Neil Rhodes",
+        email: "rhodes@hmc.edu",
+        phone: "(909) 555-1212"
+    },
+    {
+        id: 787,
+        name: "Barack Obama",
+        email: "ex-prez@whitehouse.gov",
+        phone: "(312) 555-1212"
+    }
+];
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <div id="container">
+        <b>People</b>
+        {data.map(e =>
+            <div id="para">
+                <p id="name"><b>{e.name}</b></p>
+                <p id="email">{e.email}</p>
+                <p id="phone">{e.phone}</p>
+            </div>
+        )}
+    </div>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
